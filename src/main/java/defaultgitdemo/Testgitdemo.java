092186e1841308162dbfa06e01540a90c281a838
sumi81 <113192198+sumi81@users.noreply.github.com>
2026-07-07 18:09:44 -0700

    First commit....

---
+++ src/main/java/defaultgitdemo/Testgitdemo.java
@@ -0,0 +1,14 @@
+package defaultgitdemo;
+
+public class Testgitdemo {
+
+	public static void main(String[] args) {
+		String hungry ="yes";
+		if(hungry == "yes")
+			System.out.print("eat something");
+		else {
+			System.out.println("do work....");
+		}
+	}
+
+}
